065bfb0ce6f0652e30cd6e8454d530e1125cfeed
John Watson <johnwatson484@hotmail.com>
2022-02-27 13:30:25 +0000

    Update logs

--- app/index.js
+++ app/index.js
@@ -1,12 +1,17 @@
 const axios = require('axios')
+const CONTAINER_NAME = 'batch'
 
 module.exports = async function (context, myBlob) {
   context.log('JavaScript blob trigger function processed blob \n Blob:', context.bindingData.blobTrigger, '\n Blob Size:', myBlob.length, 'Bytes')
-  const sourceFile = context.bindingData.blobTrigger
+  const sourceFile = context.bindingData.blobTrigger.replace(`${CONTAINER_NAME}/`, '')
 
   const result = await axios.post(process.env.TRIGGER_URL, {
     sourceFile
   })
 
-  console.log(result)
+  if (result.status === 200) {
+    console.log(`Successfully triggered transfer for ${sourceFile}`)
+  } else {
+    console.log(`Unable to transfer ${sourceFile}`)
+  }
 }
