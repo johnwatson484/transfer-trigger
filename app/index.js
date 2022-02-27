8ff8d43bf33920733fca7b11ed7739559976ce5c
John Watson <johnwatson484@hotmail.com>
2022-02-27 13:15:24 +0000

    Add webhook

--- app/index.js
+++ app/index.js
@@ -2,5 +2,11 @@ const axios = require('axios')
 
 module.exports = async function (context, myBlob) {
   context.log('JavaScript blob trigger function processed blob \n Blob:', context.bindingData.blobTrigger, '\n Blob Size:', myBlob.length, 'Bytes')
-  const file = context.bindingData.blobTrigger
+  const sourceFile = context.bindingData.blobTrigger
+
+  const result = await axios.post(process.env.TRIGGER_URL, {
+    sourceFile
+  })
+
+  console.log(result)
 }
